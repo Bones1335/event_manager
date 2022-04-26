5c2b2bcc0cb718b19b459d29f3ee401a8df55fb5
Alexander <ahansen1335@gmail.com>
2022-04-26 09:41:57 +0200

    create common day and hour methods

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,8 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'time'
+require 'date'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
@@ -42,6 +44,44 @@ def clean_phone_number(phone_number)
   end
 end
 
+def most_common_reg_hour
+  contents = CSV.open(
+    'event_attendees.csv',
+    headers: true,
+    header_converters: :symbol
+  )
+  reg_day_hour_arr = []
+  contents.each do |row|
+    date_time = row[:regdate]
+    reg_hour = Time.strptime(date_time, "%m/%d/%Y %k:%M").hour
+    reg_day_hour_arr.push(reg_hour)
+  end
+  most_common_hour = reg_day_hour_arr.reduce(Hash.new(0)) do |hash, hour|
+    hash[hour] += 1
+    hash
+  end
+  most_common_hour.max_by { |_k, v| v }[0]
+end
+
+def most_common_reg_day
+  contents = CSV.open(
+    'event_attendees.csv',
+    headers: true,
+    header_converters: :symbol
+  )
+  reg_day_day_arr = []
+  contents.each do |row|
+    date_time = row[:regdate]
+    reg_day = Date.strptime(date_time, "%m/%d/%Y %k:%M").strftime('%A')
+    reg_day_day_arr.push(reg_day)
+  end
+  most_common_day = reg_day_day_arr.reduce(Hash.new(0)) do |hash, day|
+    hash[day] += 1
+    hash
+  end
+  most_common_day.max_by { |_k, v| v }[0]
+end
+
 puts 'EventManager Initialized!'
 
 contents = CSV.open(
@@ -58,17 +98,23 @@ contents.each do |row|
   name = row[:first_name]
   phone_number = clean_phone_number(row[:homephone])
 
+  date_time = row[:regdate]
+
+  day = Date.strptime(date_time, "%m/%d/%Y %k:%M").strftime('%A')
+
   zipcode = clean_zipcode(row[:zipcode])
 
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
 
-  save_thank_you_letter(id, form_letter)
+  # save_thank_you_letter(id, form_letter)
 
-  puts "#{name} #{phone_number}"
+  puts "#{day} #{name} #{phone_number}"
 end
 
+puts "This is the most common hour of registration: #{most_common_reg_hour}:00."
+puts "This is the most common day of registration: #{most_common_reg_day}."
 # Everything bellow this line is building a CSV parser from scratch just to understand how it's done.
 
 # contents = File.read('event_attendees.csv')
